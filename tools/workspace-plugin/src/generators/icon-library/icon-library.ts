83a8229955d4bcdc61dadfd55df2af59f534fab5
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-04-01 21:57:30 -0500

    feat: add set new iconset

--- tools/workspace-plugin/src/generators/icon-library/icon-library.ts
+++ tools/workspace-plugin/src/generators/icon-library/icon-library.ts
@@ -2,12 +2,14 @@ import { formatFiles, Tree } from '@nx/devkit';
 
 import { IconLibraryGeneratorSchema } from './schema';
 import { generateIconLibrary } from './lib/generate-icon-library';
+import { addIconSet } from './lib/add-iconset';
 
 export async function iconLibraryGenerator(
   tree: Tree,
   options: IconLibraryGeneratorSchema
 ) {
   await generateIconLibrary(tree, options);
+  addIconSet(tree, options);
   await formatFiles(tree);
 }
 
