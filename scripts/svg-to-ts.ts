79632a4a98388f723490148ebe6de75b5a5ae8fe
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-04-18 20:21:35 -0500

    feat: add default export when icons is empty

--- scripts/svg-to-ts.ts
+++ scripts/svg-to-ts.ts
@@ -99,6 +99,13 @@ async function createIconset(collection: IconifyJSON) {
   progressBar.update(iconEntries.length, { name: '' })
   progressBar.stop()
 
+  if (!globalIndex.length) {
+    globalIndex.push(`// Workaround for: https://github.com/microsoft/rushstack/issues/2806.
+// This is a private export that can be removed at any time.
+export const ɵɵtsModuleIndicatorApiExtractorWorkaround = true
+`)
+  }
+
   await fs.writeFile(`${libraryDir}/index.ts`, globalIndex.sort().join('\n'))
 }
 
