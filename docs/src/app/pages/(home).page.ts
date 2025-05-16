20727826f7a1c796dbab3b8615c0d3544e1935ad
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-05-15 21:41:36 -0500

    feat: add docs page

---
+++ docs/src/app/pages/(home).page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'docs-home',
+  imports: [],
+  template: ``,
+})
+export default class HomeComponent {}
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,6 @@
 import antfu from '@antfu/eslint-config'
-import { collections } from './collections/src/lib/collections'
 
 export default antfu({
   formatters: true,
-  astro: true,
-  ignores: [
-    ...collections.flatMap(collection => [
-      `packages/${collection}/**/index.ts`,
-      `packages/${collection}/**/chunks/*.ts`,
-    ]),
-    'docs/.astro',
-  ],
+  unocss: true,
 })
